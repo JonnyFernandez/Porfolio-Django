484c1f9825e46fba57f6d3591d69ab31bbc4faf0
JonnyFernandez <arcancode@gmail.com>
2024-09-09 17:41:38 -0300

    mejorando seguridad2

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!vn=n-eer_5qlkt+adk+x$zdokz#dr4w0!7lq#+49l*s=wv6a3'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'clave-por-defecto')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
